83a95318f7f842500330d88ca5884261b903cff3
lilywhale <94987363+lilywhale@users.noreply.github.com>
2023-05-12 14:45:32 -0400

    Update chatbot_streamlit.py

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -11,10 +11,10 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('C:/Users/mimil/programmation/python_projects/chatbot_project/intents_final1.json', 'r') as json_data:
+with open('intents_final1.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "C:/Users/mimil/programmation/python_projects/chatbot_project/data1.pth"
+FILE = "data1.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
